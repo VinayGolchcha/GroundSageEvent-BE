c5d127e2d9990e3e4f6dfd10a2642460f86c0f10
Sanjana_Jain022 <sanjana.jain@shephertz.com>
2024-04-22 17:29:05 +0530

    # referral code logic implemented and tested along with validation

--- v1/event/routes/routes.js
+++ v1/event/routes/routes.js
@@ -3,14 +3,15 @@ const app = express()
 const router = Router();
 
 import authenticateToken from '../../../middlewares/auth.js';
-import {createEventTeamAndReferralCode, getAllEvents, updateEvent, deleteEvent} from '../controller/eventController.js';
-import {createEventVal, updateEventVal, deleteEventVal} from '../../../utils/validation.js';
+import {createEventTeamAndReferralCode, getAllEvents, updateEvent, deleteEvent, joinUserTeamWithReferralCode} from '../controller/eventController.js';
+import {createEventVal, updateEventVal, deleteEventVal, joinEventTeamVal} from '../../../utils/validation.js';
 router.use(authenticateToken)
 
 app.post('/create-event-team-and-referral-code',createEventVal, createEventTeamAndReferralCode);
 app.post('/update-event/:id',updateEventVal, updateEvent);
 app.get('/get-all-event', getAllEvents);
 app.delete('/delete-event/:id',deleteEventVal, deleteEvent);
+app.post('/join-team-with-referral-code', joinEventTeamVal, joinUserTeamWithReferralCode);
 
 app.use("/", router);
 
