4544dbd34eb2fca7181678f2759893a47e82acaa
shubham soni <ssoni190@gmail.com>
2024-06-24 16:20:50 +0530

    #bug fixes

--- v1/shop/model/shopQuery.js
+++ v1/shop/model/shopQuery.js
@@ -42,7 +42,11 @@ export const updateShopQuery = (query, array)=> {
 export const getAllShopsByEventIdQuery = async(array) =>{
     try {
         let query = `
-        SELECT s.*, ra.end_date
+        SELECT s.*, 
+            CASE 
+                WHEN s.status = 'occupied' THEN ra.end_date 
+                ELSE NULL 
+            END AS end_date
         FROM shops s
         LEFT JOIN (
             SELECT shop_id, MAX(end_date) AS end_date
@@ -76,7 +80,7 @@ export const deleteShopQuery = async(array)=>{
     }
 }
 
-export const getShopOccupancyDetailsQuery = async(flag, array)=>{
+export const getShopOccupancyDetailsQuery = async (flag, array) => {
     try {
         let query;
         if (flag === 'shop') {
@@ -84,13 +88,13 @@ export const getShopOccupancyDetailsQuery = async(flag, array)=>{
             SELECT 
                 s.id AS shop_id, 
                 s.shop_number,
-                CASE WHEN s.status = 'available' THEN 'no' ELSE 'yes' END AS rented,
-                CASE WHEN s.status = 'available' THEN 'yes' ELSE 'no' END AS vacant
+                CASE WHEN s.status = 'vacant' THEN 'no' ELSE 'yes' END AS rented,
+                CASE WHEN s.status = 'vacant' THEN 'yes' ELSE 'no' END AS vacant
             FROM 
                 shops s
             LEFT JOIN 
                 rentalagreements ra ON s.id = ra.shop_id AND ra.event_id = ?
-            JOIN 
+            LEFT JOIN 
                 (SELECT 
                     shop_id,
                     MAX(created_at) AS latest_created_at
@@ -100,17 +104,17 @@ export const getShopOccupancyDetailsQuery = async(flag, array)=>{
                     shop_id) latest_rental ON ra.shop_id = latest_rental.shop_id AND ra.created_at = latest_rental.latest_created_at
             WHERE 
                 s.event_id = ?;
-          `;
+            `;
         } else if (flag === 'month') {
             query = `
             SELECT 
                 CONCAT(MONTHNAME(m.month), ' ', YEAR(m.month)) AS month_year,
                 COUNT(DISTINCT s.id) AS total_shops,
-                COUNT(DISTINCT CASE WHEN s.status = 'available' THEN NULL ELSE s.id END) AS occupied,
-                COUNT(DISTINCT CASE WHEN s.status = 'available' THEN s.id ELSE NULL END) AS vacant
+                COUNT(DISTINCT CASE WHEN s.status = 'vacant' THEN NULL ELSE s.id END) AS occupied,
+                COUNT(DISTINCT CASE WHEN s.status = 'vacant' THEN s.id ELSE NULL END) AS vacant
             FROM 
                 (SELECT 
-                    ADDDATE('2024-01-01', INTERVAL m MONTH) AS month
+                    ADDDATE(CONCAT(YEAR(CURDATE()), '-01-01'), INTERVAL m MONTH) AS month
                 FROM 
                     (SELECT @row := @row + 1 AS m
                     FROM (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 UNION SELECT 10 UNION SELECT 11 UNION SELECT 12) t1,
@@ -120,9 +124,7 @@ export const getShopOccupancyDetailsQuery = async(flag, array)=>{
             LEFT JOIN 
                 rentalagreements ra ON YEAR(ra.start_date) = YEAR(m.month) AND MONTH(ra.start_date) <= MONTH(m.month) AND YEAR(ra.end_date) = YEAR(m.month) AND MONTH(ra.end_date) >= MONTH(m.month) AND ra.event_id = ?
             LEFT JOIN 
-                shops s ON s.id = ra.shop_id
-            WHERE
-                ra.event_id = ?
+                shops s ON s.id = ra.shop_id AND s.event_id = ?
             GROUP BY
                 CONCAT(MONTHNAME(m.month), ' ', YEAR(m.month)),
                 YEAR(m.month),
@@ -132,14 +134,15 @@ export const getShopOccupancyDetailsQuery = async(flag, array)=>{
             ORDER BY
                 YEAR(m.month),
                 MONTH(m.month);
-          `;
+            `;
         }
         return pool.query(query, array);
     } catch (error) {
         console.error("Error executing getShopOccupancyDetailsQuery:", error);
-        throw err
+        throw error;
     }
 }
+
 
 export const getLastActivityIdQuery = () =>{
     let query = `SELECT id FROM shops ORDER BY id DESC LIMIT 1`
